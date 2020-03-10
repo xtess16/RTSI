4613bd164ad0fa6cae28d36b2ec75bd7cb12108c
xtess <brikevgeny@yandex.ru>
2020-03-10 18:15:20 +0300

    Пока не начал переписывать тетрадку

--- statistics.py
+++ statistics.py
@@ -1,5 +1,5 @@
 import functools
-from typing import Union, Tuple, Any
+from typing import Union, Tuple, Any, Sequence
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,7 +8,6 @@ from statsmodels.tsa.stattools import adfuller, pacf, acf
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.stats.diagnostic import het_white, het_breuschpagan
 import numpy as np
-from numpy import polyfit
 from scipy import stats
 import RegscorePy as rp
 from numpy import sqrt
@@ -17,22 +16,22 @@ import itertools
 import chow_test
 from tqdm import tqdm
 
-
 draw = plt.plot
+T_SEQUENCE = Union[Sequence, np.ndarray]
 
 
-def exponential_smoothing(series, alpha):
-    result = np.array([series[0]])  # first value is same as series
-    for n in range(1, len(series)):
-        result = np.append(result, alpha * series[n] + (1 - alpha) * result[n - 1])
+def exponential_smoothing(seq, alpha):
+    result = np.array([seq[0]])  # first value is same as series
+    for n in range(1, len(seq)):
+        result = np.append(result, alpha * seq[n] + (1 - alpha) * result[n - 1])
 
     res = 0
-    for s, r in zip(series, result):
+    for s, r in zip(seq, result):
         res += (s - r) ** 2
     return result
 
 
-def find_aic(array1: Union[list, np.ndarray], array2: Union[list, np.ndarray], k: int = 5):
+def find_aic(array1: Union[list, np.ndarray], array2: Union[list, np.ndarray], k: int = 1):
     """
         Поиск КФ Акаике
     :param array1: Первый список
@@ -67,7 +66,7 @@ def find_bic(array1: Union[list, np.ndarray], array2: Union[list, np.ndarray], k
 
 
 def get_polyfit(seq, k: int) -> Tuple[np.ndarray, Any, np.ndarray]:
-    p = polyfit(range(len(seq)), seq, k)
+    p = np.polyfit(range(len(seq)), seq, k)
     return p
 
 
@@ -91,8 +90,8 @@ def find_determination_kf(seq, k):
 
 def find_adjusted_determination_kf(seq, k):
     determination_kf = find_determination_kf(seq, k)
-    # XXX -5
-    kf = 1 - (1 - determination_kf) * ((len(seq) - 1) / (len(seq) - 5))
+    # XXX k
+    kf = 1 - (1 - determination_kf) * ((len(seq) - 1) / (len(seq) - k))
     return kf
 
 
@@ -111,6 +110,113 @@ def find_acf(seq, a, b=None):
         res_lag = pearsonr(seq[: -lag], seq[lag:])[0]
         lags.append(res_lag)
     return lags
+
+
+class Model:
+    def __init__(self, seq: T_SEQUENCE):
+        self.sequence: np.ndarray = np.array(seq)
+
+    @property
+    def seq(self):
+        return self.sequence
+
+    @seq.setter
+    def seq(self, value: T_SEQUENCE):
+        self.sequence = value
+
+    def __iter__(self):
+        return iter(self.sequence)
+
+    def arima(self, *args, **kwargs):
+        return Arima(self.sequence, *args, **kwargs)
+
+    def acf(self, lag: int):
+        """
+            Поиск автокорреляции
+        :param lag: Значение лага
+        :return: Список/Значение корреляции
+        """
+        return pearsonr(self.sequence[:-lag], self.sequence[lag:])[0]
+
+    def many_acf(self, lag_from: int, lag_to: int):
+        """
+            Поиск автокорреляции по нескольким лагам
+        :param lag_from: Поиск ОТ лага
+        :param lag_to: Поиск ДО лага
+        :return: Список/Значение корреляции
+        """
+        lags = []
+        for lag in range(lag_from, lag_to + 1):
+            res_lag = pearsonr(self.sequence[: -lag], self.sequence[lag:])[0]
+            lags.append(res_lag)
+        return lags
+
+    def aic(self, array: T_SEQUENCE, k: int = 1) -> float:
+        """
+            Поиск КФ Акаике
+        :param array: Второй список
+        :param k: Количество параметров
+        :return: КФ Акаике
+        """
+        return rp.aic.aic(list(self.sequence), list(array), k) / len(self.sequence)
+
+    def find_bic(self, array: T_SEQUENCE, k: int = 1):
+        """
+            Вычисление критерия Шварца
+        :param array: Второй список
+        :param k: Количество параметров
+        :return: Критерий Шварца
+        """
+        return rp.bic.bic(list(self.sequence), list(array), k) / len(self.sequence)
+
+    def trend_by_kf(self, p):
+        if isinstance(p, int):
+            p = self.polyfit(p)
+        pf = np.poly1d(p)
+        return Model(pf(range(len(self.sequence))))
+
+    def polyfit(self, k: int):
+        p = np.polyfit(range(len(self.sequence)), self.sequence, k)
+        return p
+
+    def determination_kf(self, k) -> float:
+        """
+            Подсчет КФ детерминации
+        :param k: Количество параметров модели
+        :return: КФ детерминации
+        """
+        pl = self.trend_by_kf(self.polyfit(k)).sequence
+        mean = self.sequence.mean()
+        top = ((pl - mean) ** 2).sum()
+        bottom = ((self.sequence - mean) ** 2).sum()
+        determination = top / bottom
+        return determination
+
+    def adjusted_determination_kf(self, k):
+        """
+            Подсчет скорректированного КФ детерминации
+        :param k: Количество параметров модели
+        :return: Скорректированный КФ детерминации
+        """
+        determination_kf = find_determination_kf(self.sequence, k)
+        # XXX k
+        kf = 1 - (1 - determination_kf) * ((len(self.sequence) - 1) / (len(self.sequence) - k))
+        return kf
+
+    def exponential_smoothing(self, alpha):
+        """
+            Экспоненциальное сглаживание
+        :param alpha: КФ
+        :return:
+        """
+        # TODO: найти либу
+        result = np.array([self.sequence[0]])
+        for n in range(1, len(self.sequence)):
+            result = np.append(result, alpha * self.sequence[n] + (1 - alpha) * result[n - 1])
+        res = 0
+        for s, r in zip(self.sequence, result):
+            res += (s - r) ** 2
+        return Model(result)
 
 
 class Arima:
@@ -153,9 +259,3 @@ class Arima:
 
     def __getattr__(self, item):
         return getattr(self.model, item)
-
-
-if __name__ == '__main__':
-    series = [i for i in pd.read_csv('./data/RTS-history-USA.csv')[::25].Open]
-    model, fitted = Arima.find_optimal_model_by_order(series, range(5), (0, 1), range(3))
-    print(fitted.aic)
