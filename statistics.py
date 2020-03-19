b53c4dc0a67b14548be2bc91e01e528eefced7ae
xtess <brikevgeny@yandex.ru>
2020-03-19 17:44:12 +0300

    Кусочный тренд. Ч1

--- statistics.py
+++ statistics.py
@@ -1,7 +1,5 @@
 import functools
 import itertools
-import queue
-import threading
 import time
 import traceback
 from multiprocessing.pool import Pool
@@ -11,17 +9,16 @@ import RegscorePy as rp
 import chow_test
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import scipy
+import statsmodels.api as sm
 from scipy.stats.stats import pearsonr
+from sklearn.preprocessing import PolynomialFeatures
 from statsmodels.tsa.arima_model import ARIMA
-from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.tsa.stattools import adfuller, pacf, acf
-from statsmodels.stats.diagnostic import het_white
 from tqdm import tqdm
-import warnings
+import pandas as pd
 
-T_SEQUENCE = Union[Sequence, np.ndarray]
+T_SERIES = Union[pd.Series, 'Model']
 
 
 def draw(*args, **kwargs):
@@ -29,9 +26,9 @@ def draw(*args, **kwargs):
     result_args = []
     for arg in args:
         if isinstance(arg, Model):
-            result_args.append(arg.sequence)
+            result_args.append(arg.series)
         elif isinstance(arg, PartialModel):
-            for s in arg.sequence_for_draw():
+            for s in arg.draw_series():
                 draw(s, **kwargs)
             return
         else:
@@ -40,59 +37,56 @@ def draw(*args, **kwargs):
 
 
 class Model:
-    def __init__(self, seq: T_SEQUENCE, k=None):
-        if isinstance(seq, PartialModel):
-            seq = seq.join()
-        self.sequence: np.ndarray = np.array(seq)
+    def __init__(self, series: T_SERIES, k=None):
+        if isinstance(series, PartialModel):
+            series = series.join()
+        elif not isinstance(series, pd.Series):
+            raise TypeError('Аргумент series может быть только PartialModel или pd.Series')
+        self.series: pd.Series = series
         self.k = k
 
-    @property
-    def seq(self):
-        return self.sequence
-
-    @seq.setter
-    def seq(self, value: T_SEQUENCE):
-        self.sequence = value
-
     def __getattr__(self, item):
-        return getattr(self.sequence, item)
+        res = getattr(self.series, item)
+        if isinstance(res, pd.Series):
+            return Model(res)
+        return res
 
     def __getitem__(self, item):
-        return Model(self.sequence[item])
+        return Model(self.series[item])
 
     def __iter__(self):
-        return iter(self.sequence)
+        return iter(self.series)
 
     def __sub__(self, other):
         if isinstance(other, Model):
-            return Model(self.sequence - other.sequence)
-        return Model(self.sequence - other)
+            return Model(self.series - other.series)
+        return Model(self.series - other)
 
     def __add__(self, other):
         if isinstance(other, Model):
-            return Model(self.sequence + other.sequence)
-        return Model(self.sequence + other)
+            return Model(self.series + other.series)
+        return Model(self.series + other)
 
     def __mul__(self, other):
         if isinstance(other, Model):
-            return Model(self.sequence * other.sequence)
-        return Model(self.sequence * other)
+            return Model(self.series * other.series)
+        return Model(self.series * other)
 
     def __pow__(self, power):
-        return Model(self.sequence ** power)
+        return Model(self.series ** power)
 
     def __truediv__(self, other):
         if isinstance(other, Model):
-            return Model(self.sequence / other.sequence)
-        return Model(self.sequence / other)
+            return Model(self.series / other.series)
+        return Model(self.series / other)
 
     def __floordiv__(self, other):
         if isinstance(other, Model):
-            return Model(self.sequence // other.sequence)
-        return Model(self.sequence // other)
+            return Model(self.series // other.series)
+        return Model(self.series // other)
 
     def __len__(self):
-        return len(self.sequence)
+        return len(self.series)
 
     def append(self, other, inplace=False):
         """
@@ -102,30 +96,25 @@ class Model:
         :return: Склеенный список
         """
         if inplace:
-            if isinstance(other, Model):
-                self.sequence = np.append(self.sequence, other.sequence)
-            else:
-                self.sequence = np.append(self.sequence, other)
+            self.series = self.series.append(other.series)
         else:
-            if isinstance(other, Model):
-                return Model(np.append(self.sequence, other.sequence))
-            return Model(np.append(self.sequence, other))
+            return Model(self.series.append(other.series))
 
     def fuller_test(self):
         """ Тест Фуллера """
-        return adfuller(self.sequence)
+        return adfuller(self.series)
 
     def durbin_watson_test(self):
         """ Тест Дарбина Уотсона """
         dw = 0
-        for t in range(1, len(self.sequence)):
-            dw += (self.sequence[t] - self.sequence[t-1]) ** 2
-        dw /= (self.sequence ** 2).sum()
+        for t in range(1, len(self.series)):
+            dw += (self.series[t] - self.series[t-1]) ** 2
+        dw /= (self.series ** 2).sum()
         return dw
 
     def arima(self, *args, **kwargs):
         """ Построение модели АРИМА """
-        return Arima(self.sequence, *args, **kwargs)
+        return Arima(self.series, *args, **kwargs)
 
     def acf(self, lag: int, **kwargs):
         """
@@ -164,7 +153,7 @@ class Model:
         """
         return np.array([self.pacf(i) for i in range(lag_from, lag_to)])
 
-    def aic(self, array: T_SEQUENCE, k: Optional[int] = None) -> float:
+    def aic(self, array: T_SERIES, k: Optional[int] = None) -> float:
         """
             Поиск КФ Акаике
         :param array: Второй список
@@ -174,12 +163,12 @@ class Model:
         if isinstance(array, Model):
             if k is None:
                 k = array.k
-            array = array.sequence.tolist()
+            array = array.series.tolist()
         elif isinstance(array, np.ndarray):
             array = array.tolist()
-        return rp.aic.aic(self.sequence.tolist(), array, k) / len(self.sequence)
+        return rp.aic.aic(self.series.tolist(), array, k) / len(self.series)
 
-    def bic(self, array: T_SEQUENCE, k: Optional[int] = None):
+    def bic(self, array: T_SERIES, k: Optional[int] = None):
         """
             Вычисление критерия Шварца
         :param array: Второй список
@@ -189,23 +178,25 @@ class Model:
         if isinstance(array, Model):
             if k is None:
                 k = array.k
-            array = array.sequence.tolist()
+            array = array.series.tolist()
         elif isinstance(array, np.ndarray):
             array = array.tolist()
-        return rp.bic.bic(self.sequence.tolist(), array, k) / len(self.sequence)
+        return rp.bic.bic(self.series.tolist(), array, k) / len(self.series)
 
-    def trend(self, p):
+    def polynomial_trend(self, degree):
         """ Построение тренда по степени полинома """
-        if isinstance(p, int):
-            p = self.polyfit(p)
-        pf = np.poly1d(p)
-        return Model(pf(range(len(self.sequence))), k=len(p))
+        pf = PolynomialFeatures(degree=degree)
+        xp = pf.fit_transform(np.arange(self.series.size)[:, np.newaxis])
+        fitted_model = sm.OLS(self.series, xp).fit()
+        trend_line = Model(fitted_model.fittedvalues)
+        trend_line.fitted_model = fitted_model
+        return trend_line
 
     def polyfit(self, k: Optional[int] = None):
         """ Параметры полинома """
         if k is None:
             k = self.k
-        p = np.polyfit(range(len(self.sequence)), self.sequence, k)
+        p = np.polyfit(range(len(self.series)), self.series, k)
         return p
 
     def determination_kf(self, k: Optional[int] = None) -> float:
@@ -216,10 +207,10 @@ class Model:
         """
         if k is None:
             k = self.k
-        pl = self.trend(self.polyfit(k)).sequence
-        mean = self.sequence.mean()
+        pl = self.trend(self.polyfit(k)).series
+        mean = self.series.mean()
         top = ((pl - mean) ** 2).sum()
-        bottom = ((self.sequence - mean) ** 2).sum()
+        bottom = ((self.series - mean) ** 2).sum()
         determination = top / bottom
         return determination
 
@@ -229,10 +220,10 @@ class Model:
         :param other: Кусочный тренд
         :return: КФ детерминации
         """
-        pl = other.sequence
-        mean = self.sequence.mean()
+        pl = other.series
+        mean = self.series.mean()
         top = ((pl - mean) ** 2).sum()
-        bottom = ((self.sequence - mean) ** 2).sum()
+        bottom = ((self.series - mean) ** 2).sum()
         determination = top / bottom
         return determination
 
@@ -244,7 +235,7 @@ class Model:
         """
         if k is None:
             k = self.k
-        return (self.determination_kf(k) / (k-1)) / ((1 - self.determination_kf(k)) / (len(self.sequence) - k))
+        return (self.determination_kf(k) / (k-1)) / ((1 - self.determination_kf(k)) / (len(self.series) - k))
 
     def adjusted_determination_kf(self, k: Optional[int] = None):
         """
@@ -255,7 +246,7 @@ class Model:
         if k is None:
             k = self.k
         determination_kf = self.determination_kf(k)
-        kf = 1 - (1 - determination_kf) * ((len(self.sequence) - 1) / (len(self.sequence) - k))
+        kf = 1 - (1 - determination_kf) * ((len(self.series) - 1) / (len(self.series) - k))
         return kf
 
     def exponential_smoothing(self, alpha):
@@ -265,30 +256,32 @@ class Model:
         :return:
         """
         # TODO: найти либу
-        result = np.array([self.sequence[0]])
-        for n in range(1, len(self.sequence)):
-            result = np.append(result, alpha * self.sequence[n] + (1 - alpha) * result[n - 1])
-        res = 0
-        for s, r in zip(self.sequence, result):
-            res += (s - r) ** 2
+        # XXX
+        result = self.series.copy()
+        for n in range(1, len(result)):
+            result[n] = alpha * result[n] + (1 - alpha) * result[n - 1]
         return Model(result)
 
-    def cut_by_chow_test(self, *args, arbitrarily: Optional[int] = None, with_result=False) -> \
-            Union[Tuple[float, 'PartialModel'], 'PartialModel']:
+    def cut_by_chow_test(self, *args, arbitrarily: Optional[int] = None, with_result=False):
         """
             Поиск оптимального разделения графика по Чоу тесту
-        :param args: Набор диапазонов
+        :param args: Набор диапазонов в которых будет происходить точек для деления
         :param arbitrarily: Если передано число,
             то метод сам разделит последовательность на arbitrarily частей
         :param with_result: Возвращать результат теста или нет
-        :return: Максимальный результат теста и индексы разделения
+        :return: Если with_result - True, то возвращает кортеж из 3 элементов:
+            Значение лучшего теста, список индексов, делящих series на части, PartialModel
         """
         # Если передано arbitrarily, то перебираем все значения
         if isinstance(arbitrarily, int):
-            args = tuple(range(len(self.sequence)) for _ in range(arbitrarily-1))
+            args = tuple(range(len(self.series)) for _ in range(arbitrarily-1))
         max_chow_result: float = 0
         max_chow_result_indexes: Optional[List[int]] = None
-        iter_count = functools.reduce(lambda x, y: x*y, map(len, args))
+        if isinstance(args[0], slice):
+            iter_count = functools.reduce(lambda x, y: x * y, map(lambda z: len(self.series[z]), args))
+            args = [self.series[s].index for s in args]
+        else:
+            iter_count = functools.reduce(lambda x, y: x*y, map(len, args))
 
         # Разделяем последовательность на части по индексам, потом сравниаем все соседние
         for cut_indexes in tqdm(
@@ -296,17 +289,17 @@ class Model:
                 total=iter_count, desc='CHOW ', unit=' test'):
             try:
                 cut_indexes = sorted(cut_indexes)
-                test_sequence = []
+                test_series = []
                 last_index = 0
                 for index in cut_indexes:
-                    test_sequence.append(self.sequence[last_index:index])
+                    test_series.append(self.series[last_index:index])
                     last_index = index
-                test_sequence.append(self.sequence[last_index:])
+                test_series.append(self.series[last_index:])
 
                 chow = 0
-                for i in range(len(test_sequence)-1):
-                    a = test_sequence[i]
-                    b = test_sequence[i+1]
+                for i in range(len(test_series)-1):
+                    a = test_series[i]
+                    b = test_series[i+1]
                     chow_test_result = chow_test.f_value(range(len(a)), a, range(len(a), len(a)+len(b)), b)
                     chow += chow_test_result[0]
                 chow /= len(cut_indexes)+1
@@ -318,20 +311,20 @@ class Model:
         models = []
         last_index = 0
         for index in max_chow_result_indexes:
-            models.append(Model(self.sequence[last_index:index]))
+            models.append(Model(self.series[last_index:index]))
             last_index = index
-        models.append(Model(self.sequence[last_index:]))
+        models.append(Model(self.series[last_index:]))
         if with_result:
-            return max_chow_result, PartialModel(*models)
+            return max_chow_result, [self.series.index[i] for i in max_chow_result_indexes], PartialModel(*models)
         return PartialModel(*models)
 
     def barlett(self):
         """ Критерий Барлетта """
-        return scipy.stats.bartlett(self.sequence, range(len(self.sequence)))
+        return scipy.stats.bartlett(self.series, range(len(self.series)))
 
     def levene(self):
         """ Критерий Левене """
-        return scipy.stats.levene(self.sequence, range(len(self.sequence)))
+        return scipy.stats.levene(self.series, range(len(self.series)))
 
     def mape(self, other):
         """ Критерий MAPE """
@@ -373,27 +366,19 @@ class PartialModel:
 
     def join(self):
         """ Объединение всех кусков в одну модель """
-        joined_models = np.array([])
+        joined_models = pd.Series()
         for model in self.models:
-            joined_models = np.append(joined_models, model.sequence)
+            joined_models = joined_models.append(model.series)
         return Model(joined_models)
 
-    def sequence_for_draw(self, optimize=True):
+    def draw_series(self):
         """
-            Получение последовательностей для рисования
-        :param optimize: Если True - то последних элемент каждой последовательности добавляется
-            перед первым элементом следующей последовательности
+            Последовательность self.models, где у каждого model в конце добавляется первое
+        значение следующего model
         """
-        offset = 0
-        last_value = None
-        for model in self.models:
-            if last_value is not None and optimize:
-                value = np.append([np.nan] * (offset-1), np.append(last_value, model.sequence))
-            else:
-                value = np.append([np.nan] * offset, model.sequence)
-            yield value
-            last_value = value[-1]
-            offset += len(model.sequence)
+        res = [self.models[i].append(self.models[i+1][:1]) for i in range(len(self.models)-1)]
+        res.append(self.models[-1])
+        return PartialModel(*res)
 
     def __getattr__(self, item):
         if item.startswith('each_'):
@@ -411,45 +396,43 @@ class PartialModel:
 class Arima:
     def __init__(self, endog, *args, **kwargs):
         if isinstance(endog, Model):
-            endog = endog.sequence
+            endog = endog.series
         self.model = ARIMA(endog, *args, **kwargs)
         self.order = tuple(kwargs.get('order'))
 
     def fit(self, *args, **kwargs):
         """ Тренировка модели """
         _fitted = self.model.fit(*args, **kwargs)
-        _fitted.sequence = _fitted.fittedvalues
-        _fitted.seq = _fitted.sequence
         # FIXME: k-?
-        _fitted.aic = Model(self.model.endog).aic(_fitted.sequence, self.order[0]+self.order[2])
+        _fitted.aic = Model(self.model.endog).aic(_fitted.series, self.order[0]+self.order[2])
         _fitted.order = self.order
         return _fitted
 
     @classmethod
-    def find_optimal_model_by_order(cls, endog, p_sequence, d_sequence, q_sequence, top=1):
+    def find_optimal_model_by_order(cls, endog, p_series, d_series, q_series, top=1):
         """
             Подбор оптимальных p, d, q для временного ряда
         :param endog: Последовательность
-        :param p_sequence: параметр p
-        :param d_sequence: параметр d
-        :param q_sequence: параметр q
+        :param p_series: параметр p
+        :param d_series: параметр d
+        :param q_series: параметр q
         :param top: Количство лучших моделей, которое надо вернуть
         :return: Натренированная модель с наименьшим aic
         """
         if isinstance(endog, Model):
-            endog = endog.sequence
-        if isinstance(p_sequence, int):
-            p_sequence = (p_sequence, )
-        if isinstance(d_sequence, int):
-            d_sequence = (d_sequence, )
-        if isinstance(q_sequence, int):
-            q_sequence = (q_sequence, )
+            endog = endog.series
+        if isinstance(p_series, int):
+            p_series = (p_series, )
+        if isinstance(d_series, int):
+            d_series = (d_series, )
+        if isinstance(q_series, int):
+            q_series = (q_series, )
 
         start = time.monotonic()
         pool = Pool()
         result = pool.map(
             cls.pdq_handler,
-            [(endog, p, d, q) for p, d, q in itertools.product(p_sequence, d_sequence, q_sequence)]
+            [(endog, p, d, q) for p, d, q in itertools.product(p_series, d_series, q_series)]
         )
         finish = time.monotonic() - start
         print(f'Перебрал {len(result)} вариант(а/ов) за {finish:.2f} сек.')
